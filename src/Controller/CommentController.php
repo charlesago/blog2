4915926cf3ca594657e14615ef2f56d929eba3b7
USERNAME <chacha.agostinelli@gmail.com>
2023-04-28 08:04:34 +0200

    Final

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Form\CommentType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/comment')]
+
+class CommentController extends AbstractController
+{
+    #[Route('/', name: 'app_comment')]
+    public function index(): Response
+    {
+        return $this->render('comment/index.html.twig', [
+
+
+        ]);
+    }
+
+    #[Route('/create/{id}', name: 'create_comment')]
+    public function create(Post $post, Request $request, EntityManagerInterface $manager): response
+    {
+
+        $comment = new Comment();
+        $formCom = $this->createForm(CommentType::class, $comment);
+        $formCom->handleRequest($request);
+        if ($formCom->isSubmitted() && $formCom->isValid()) {
+
+            $comment->setPost($post);
+            $comment->setCreatedAt(new \DateTime());
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute("show_post", ['id' => $comment->getPost()->getId()]);
+        }
+
+    }
+
+    #[Route('/delete/{id}', name: 'delete_comment')]
+    public function delete(Comment $comment, EntityManagerInterface $manager):response
+    {
+
+        if($comment){
+            $manager->remove($comment);
+            $manager->flush();
+        }
+
+        return $this->redirectToRoute("show_post", ['id' => $comment->getPost()->getId()]);
+    }
+
+    #[Route('/update/{id}', name: 'update_comment')]
+    public function update(Comment $comment,  Request $request, EntityManagerInterface $manager): response
+    {
+
+        $formCom = $this->createForm(CommentType::class, $comment);
+        $formCom->handleRequest($request);
+        if ($formCom->isSubmitted() && $formCom->isValid()) {
+
+
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute("show_post", ['id' => $comment->getPost()->getId()]);
+        }
+        return $this->renderForm('comment/update.html.twig', [
+                'formCom'=>$formCom
+
+        ]);
+    }
+}
---
+++ src/Controller/PostController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Form\CommentType;
+use App\Form\PostType;
+use App\Repository\CommentRepository;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/post')]
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'app_post')]
+    public function index(PostRepository $postRepository): Response
+    {
+
+        $posts = $postRepository->findAll();
+
+        return $this->render('post/index.html.twig', [
+            'posts'=>$posts
+        ]);
+    }
+        #[Route('/show/{id}', name: 'show_post')]
+        public function show(Post $post):response
+        {
+
+            $comment= new Comment();
+            $formCom = $this->createForm(CommentType::class, $comment);
+            return $this->renderForm('post/show.html.twig', [
+                'post'=>$post,
+                'formCom'=>$formCom
+            ]);
+
+        }
+    #[Route('/delete/{id}', name: 'delete_post')]
+    public function delete(Post $post, EntityManagerInterface $manager):response
+    {
+
+        if ($post){
+            $manager->remove($post);
+            $manager->flush();
+        }
+
+
+
+        return $this->redirectToRoute('app_post');
+
+    }
+
+    #[Route('/create/', name: 'create_post')]
+    #[Route('/update/{id}', name: 'update_post')]
+    public function create( Post $post=null,  Request $request, EntityManagerInterface $manager):response
+    {
+        $edit = false;
+
+        if ($post){
+            $edit=true;
+        }
+        if (!$edit){
+            $post = new Post();
+        }
+
+        $formCreate = $this->createForm(PostType::class, $post);
+        $formCreate->handleRequest($request);
+        if ($formCreate->isSubmitted() && $formCreate->isValid()){
+
+            if (!$edit){
+            $post->setCreatedAt(new \DateTime());
+            }
+            $manager->persist($post);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_post');
+        }
+        return $this->renderForm('post/create.html.twig', [
+            'formCreate'=>$formCreate
+        ]);
+
+    }
+    }
